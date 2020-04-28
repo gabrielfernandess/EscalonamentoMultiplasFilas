0c9acce327140dee1f6dbfc9c44898b86e8a9ed4
gabrielfernandess <gabryel.fernandes@gmail.com>
2020-04-28 01:15:05 -0300

    Add files via upload

---
+++ OSlive/Algoritimos/processo.js
@@ -0,0 +1,26 @@
+function Processo(nome, chegada, execucao, prioridade, cor, fila){
+	this.nome = nome;
+	this.chegada = chegada;
+	this.execucao = execucao;
+	this.prioridade = prioridade;
+	this.cor = cor;
+	this.tempoEspera = 0;	
+	this.tempoExecucao =0;
+	this.fila = fila;
+	// this.tempoResp = 0;
+    
+	this.incrTEspera = function() {
+		this.tempoEspera ++;
+	};
+
+	this.incrTExecucao = function() {
+
+		this.tempoExecucao +=1;
+
+		if(this.tempoExecucao >= this.execucao){
+			return true;
+		}
+
+		return false;
+	};
+}
